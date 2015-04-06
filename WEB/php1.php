1617ce6a3c06caea99f724885a921d863dda54da
ferrarihelder <estevaovga2015@outlook.com>
2015-04-06 10:38:37 -0300

    Create php1.php

---
+++ WEB/php1.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-br">
+     <head>
+          <meta charset="utf-8";>
+          <title> Example </title>
+     </head>
+          <body>
+                <?php 
+                     echo "Olá, Eu sou O scriptzão";
+                     echo "<br>";
+                     $foo="0"; echo $foo;
+                     echo "<br>";
+                     $foo+=2; echo $foo;
+                     echo "<br>";
+                     $foo=$foo+13; echo $foo;
+                     echo "<br>";
+                     $foo=5+"10 little pigs"; echo $foo;
+                     echo "<br>";
+                     $foo=5+"10 small little pigs"; echo $foo;
+                     echo "<br>";
+                     $a= 'car'; echo $a;
+                     echo "<br>";
+                     $a[0]= 'b'; echo $a; 
+                     echo "<br>";
+                     echo $a; 
+                     echo "<br>";
+                     $foo= 10; echo $foo; 
+                     echo "<br>";
+                     $bar =(boolean) $foo; echo $foo; 
+                     echo "<br>";
+                      
+                ?>
+                
+          </body>
+
+
+</html>
